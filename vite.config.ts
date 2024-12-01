5735daebae9c04baab7b20e15fe4af2fb3bf4612
Black Magic <dlyoungworking@gmail.com>
2024-12-01 10:58:37 -0800

    updated servers

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   envDir: './environment',
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
+    open: process.env.NODE_ENV !== 'production',
   },
 });
