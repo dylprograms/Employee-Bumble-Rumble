1ae5387b38cb5c278f0c3ddcb847b923c3272375
Black Magic <dlyoungworking@gmail.com>
2024-12-01 11:13:31 -0800

    updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 3000,
